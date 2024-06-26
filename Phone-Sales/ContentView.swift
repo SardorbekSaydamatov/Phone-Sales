1e18519efb67b2cfd1e068013808da26070b6e2c
Sardorbek <SardorbekSaydamatov>
2024-06-26 19:34:13 +0500

    Initial Commit

---
+++ Phone-Sales/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Phone-Sales
+//
+//  Created by Sardorbek Saydamatov on 26/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Phone-Sales/Phone_SalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Phone_SalesApp.swift
+//  Phone-Sales
+//
+//  Created by Sardorbek Saydamatov on 26/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Phone_SalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
